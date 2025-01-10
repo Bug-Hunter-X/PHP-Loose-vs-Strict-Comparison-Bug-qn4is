fb076ef4ec5feb3a0f0003fba23fa3a931c3286a
BugHunter-X <askzenai@gmail.com>
2025-01-10 17:08:05 +0100

    Add bug.php file

---
+++ bug.php
@@ -0,0 +1,16 @@
+function foo(a, b) {
+  if (a === b) {
+    return true; 
+  }
+  return false; 
+}
+
+function bar(a, b) {
+  if (a == b) {
+    return true;
+  }
+  return false;
+}
+
+console.log(foo(0, '0')); // true
+console.log(bar(0, '0')); // true
